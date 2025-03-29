5626ca46ecd8d3adf449448fe1b896ac26c79f9a
Shok48 <145995576+Shok48@users.noreply.github.com>
2025-03-29 10:31:18 +0300

    Реализована логика перенумеровки графа

--- src/pages/HierarchyPage/HierarchyPage.jsx
+++ src/pages/HierarchyPage/HierarchyPage.jsx
@@ -1,16 +1,16 @@
 import Page from '../Page/Page';
 import styles from './HierarchyPage.module.css';
 import IncidenceInput from '../../components/InsidenceInput/IncidenceInput';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { convertLefIncData, getHierarchyLevels } from '../../utils/graphUtils';
 import { Button } from 'antd';
 import { PlayCircleOutlined } from '@ant-design/icons';
-
 
 const HierarchyPage = () => {
     const title = 'Выделение иерархических уровней';
     const [graphData, setGraphData] = useState(null);
     const [hierarchyGraph, setHierarchyGraph] = useState(null);
+    const [mapping, setMapping] = useState(null);
     const [incData, setIncData] = useState({});
 
     const handleDataChange = (data) => {
@@ -24,6 +24,38 @@ const HierarchyPage = () => {
         setGraphData(graphData);
         const hierarchy = getHierarchyLevels(graphData);
         console.log('Иерархические уровни', hierarchy);
+        const { newGraphData, mapping } = renumberGraph(graphData, hierarchy);
+        console.log('newGraphData', newGraphData);
+        setHierarchyGraph(newGraphData);
+        console.log('mapping', mapping);
+        setMapping(mapping);
+    }
+
+    const renumberGraph = (data, HL) => {
+        const { nodes, edges } = data;
+
+        const nodeMap = new Map();
+        let newId = 1;
+        for (const level of HL) {
+            for (const node of level.level) {
+                nodeMap.set(node, newId++);
+            }
+        }
+
+        const newEdges = edges.map((edge) => ({
+            source: nodeMap.get(edge.source),
+            target: nodeMap.get(edge.target),
+        }))
+
+        const newGraphData = {
+            nodes: Array.from(nodeMap.values()),
+            edges: newEdges,
+        }
+
+        return { 
+            newGraphData,
+            mapping: nodeMap
+        }
     }
 
     return (
